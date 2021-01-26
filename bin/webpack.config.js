fb354b8baa09fac2460db89f9d7ccd9055a72efe
Christopher Geary <hello@crgeary.com>
2021-01-26 14:44:19 +0000

    Add webpack via webpack-cli

---
+++ bin/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    stats: 'minimal',
+    entry: {
+        main: './src/js/main.js',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    plugins: [new MiniCssExtractPlugin()],
+};
