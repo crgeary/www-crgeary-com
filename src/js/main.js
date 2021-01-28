99e354aed9b112feaebb332fd66786ce72db6bc5
Christopher Geary <hello@crgeary.com>
2021-01-28 13:33:53 +0000

    Add Figma(ish) focus states

--- src/js/main.js
+++ src/js/main.js
@@ -25,8 +25,41 @@ import '../sass/main.scss';
                 const newTheme = body.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
                 window.localStorage.setItem('color-scheme', newTheme);
                 switchTheme(newTheme);
-                // @todo: update #theme-switch with relevant theme info
             });
         }
     });
+
+    const matchesSelector = (node, selector) => {
+        try {
+            return node.matches.call(node, selector);
+        } catch (error) {
+            return false;
+        }
+    };
+
+    const focusMarker = document.querySelector('#focus-marker');
+
+    const removeFocusMarker = () => {
+        if (document.body.contains(focusMarker)) {
+            focusMarker.style.removeProperty('transform');
+            focusMarker.style.removeProperty('width');
+            focusMarker.style.removeProperty('height');
+            focusMarker.style.opacity = 0;
+        }
+    };
+    window.addEventListener('blur', removeFocusMarker, true);
+
+    const addFocusMarker = (e) => {
+        if (!(e.target instanceof HTMLElement) || !matchesSelector(e.target, ':focus-visible')) {
+            return;
+        }
+        const rect = e.target.getBoundingClientRect();
+        focusMarker.dataset.width = Math.ceil(rect.width);
+        focusMarker.dataset.height = Math.ceil(rect.height);
+        focusMarker.style.opacity = 1;
+        focusMarker.style.transform = `translate(${Math.round(rect.left)}px, ${Math.round(rect.top)}px)`;
+        focusMarker.style.width = `${Math.round(rect.width)}px`;
+        focusMarker.style.height = `${Math.round(rect.height)}px`;
+    };
+    window.addEventListener('focus', addFocusMarker, true);
 })();
